7f284290100deccdef99a6cd9a66f786b5028915
m4gii <M4gii.0918@gmail.com>
2021-03-04 15:39:43 +0800

    WEB

---
+++ animation.js
@@ -0,0 +1,23 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+const swiper = new Swiper(...);
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
